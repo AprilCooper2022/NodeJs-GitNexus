17cd97e003ba51be70d51c9664e1d095097b4249
AprilCooper2022 <aprilcooper771@gmail.com>
2026-06-27 02:51:36 +0530

    nodejs app added

---
+++ app/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "CI/CD Pipeline Working!"
+  });
+});
+
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
+}
